86167753d4a181ed86298d6a887f862625b06acf
yvvyoon <yoon4480@naver.com>
2020-02-29 21:58:35 +0900

    Change module name from api_handler.py

--- components/weather_getter.py
+++ components/weather_getter.py
@@ -1,10 +1,11 @@
 import os
 import requests
 from dotenv import load_dotenv
+from components.location_getter import
 
 load_dotenv(verbose=True)
 
-API_KEY = os.getenv('WEATHER_APP_KEY')
+API_KEY = os.getenv('WEATHER_API_KEY')
 API_URL = f'http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}&units=metric'
 
-requests.get()
+requests.get(url=API_URL)
