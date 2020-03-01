4e6889f010165b5b7715ac02c48bd22be614954b
yvvyoon <yoon4480@naver.com>
2020-03-01 23:53:04 +0900

    Functionalize

--- components/location_getter.py
+++ components/location_getter.py
@@ -9,13 +9,16 @@ load_dotenv(verbose=True)
 API_KEY = os.getenv('LOCATION_API_KEY')
 API_URL = f'https://www.googleapis.com/geolocation/v1/geolocate?key={API_KEY}'
 
-request_data = {
-    'considerIp': True,
-}
 
-api_call_result = requests.post(url=API_URL, data=request_data)
-location_data = json.loads(api_call_result.text)
-latitude = location_data['location']['lat']
-longitude = location_data['location']['lng']
+def get_coordinate():
+    request_data = {
+        'considerIp': True,
+    }
 
-# print(latitude, longitude)
+    api_call_result = requests.post(url=API_URL, data=request_data)
+
+    location_data = json.loads(api_call_result.text)
+    latitude = location_data['location']['lat']
+    longitude = location_data['location']['lng']
+
+    return latitude, longitude
