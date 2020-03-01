f2e8ffac7a97e02d5da5b3504ffd22c0eb9ab62a
yvvyoon <yoon4480@naver.com>
2020-03-01 23:53:58 +0900

    Functionalize

--- components/weather_getter.py
+++ components/weather_getter.py
@@ -8,13 +8,27 @@ import location_getter
 
 load_dotenv(verbose=True)
 
-latitude = location_getter.latitude
-longitude = location_getter.longitude
-
 API_KEY = os.getenv('WEATHER_API_KEY')
 API_URL = f'http://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&APPID={API_KEY}&units=metric'
 
-api_call_result = requests.get(url=API_URL)
-weather_data = json.loads(api_call_result.text)
+latitude = location_getter.get_coordinate()[0]
+longitude = location_getter.get_coordinate()[1]
 
-print(weather_data)
+
+def get_weather_data():
+    api_call_result = requests.get(url=API_URL)
+    weather_data = json.loads(api_call_result.text)
+
+    temperature = weather_data['main']['temp']
+    sensible_temperature = weather_data['main']['feels_like']
+    humidity = weather_data['main']['humidity']
+    wind_speed = weather_data['wind']['speed']
+    measurement_datetime = weather_data['timezone']
+
+    print(temperature)
+    print(sensible_temperature)
+    print(humidity)
+    print(wind_speed)
+    print(measurement_datetime)
+
+    return weather_data
