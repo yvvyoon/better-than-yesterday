b85f415d36786bd91537da997fe896c0a09805bd
yvvyoon <yoon4480@naver.com>
2020-03-01 20:06:44 +0900

    Success to get weather data

--- components/weather_getter.py
+++ components/weather_getter.py
@@ -1,11 +1,20 @@
 import os
 import requests
+import json
+
 from dotenv import load_dotenv
-from components.location_getter import
+
+import location_getter
 
 load_dotenv(verbose=True)
 
-API_KEY = os.getenv('WEATHER_API_KEY')
-API_URL = f'http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}&units=metric'
+latitude = location_getter.latitude
+longitude = location_getter.longitude
 
-requests.get(url=API_URL)
+API_KEY = os.getenv('WEATHER_API_KEY')
+API_URL = f'http://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&APPID={API_KEY}&units=metric'
+
+api_call_result = requests.get(url=API_URL)
+weather_data = json.loads(api_call_result.text)
+
+print(weather_data)
